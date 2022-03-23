79effd94d095201c96b3e75f341c1707adac7a5b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 21:31:05 +0300

    add badge info

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Badge } from 'reactstrap'
 
 class ProductList extends Component {
     render() {
         return (
             <div>
-                <h3 className='display-6'>Products:{this.props.currentCategory.categoryName}</h3>
+                <h3 className='display-6'>Products:<Badge color='info'>{this.props.currentCategory.categoryName}</Badge></h3>
             </div>
         )
     }
