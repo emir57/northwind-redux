8616820c1fef987535cb3f4950022eb20e5c8c8f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 17:47:10 +0300

    h3 Products

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class ProductList extends Component {
     render() {
         return (
-            <div>ProductList</div>
+            <div>
+                <h3 className='display-6'>Products</h3>
+            </div>
         )
     }
 }
