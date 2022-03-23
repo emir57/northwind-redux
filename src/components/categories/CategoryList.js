ef5c82a4efa1c3e1fcfe12b62d0e0223616b114d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 17:46:38 +0300

    h3 Categories

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class CategoryList extends Component {
     render() {
         return (
-            <div>CategoryList</div>
+            <div>
+                <h3 className='display-6'>Categories</h3>
+            </div>
         )
     }
 }
