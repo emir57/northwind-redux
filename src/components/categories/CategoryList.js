03cedbbd22206e10b8b449bab0ab3c79c968ec61
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 21:20:51 +0300

    currentCategory active

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -15,7 +15,7 @@ class CategoryList extends Component {
                 <ListGroup>
                     {
                         this.props.categories.map(category=>{
-                            return <ListGroupItem style={{cursor:"pointer"}} key={category.id}
+                            return <ListGroupItem active={category.id === this.props.currentCategory.id} style={{cursor:"pointer"}} key={category.id}
                             onClick={()=>this.props.actions.changeCategory(category)}
                             >
                                 {category.categoryName}
