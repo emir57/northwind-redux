d4dc0c853a59920a2c719d57ff203dc19f3ab662
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-24 16:10:40 +0300

    get products

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,34 +1,59 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Badge } from 'reactstrap'
+import { Badge, Table } from 'reactstrap'
 import { bindActionCreators } from 'redux'
 import * as productActions from "../../redux/actions/productActions"
 
 class ProductList extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.actions.getProducts();
     }
     render() {
         return (
             <div>
                 <h3 className='display-6'>Products:<Badge color='info'>{this.props.currentCategory.categoryName}</Badge></h3>
-                {this.props.products.length}
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Category Id</th>
+                            <th>ProductName</th>
+                            <th>Quantity Per Unit</th>
+                            <th>Unit Price</th>
+                            <th>Units In Stock</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.products.map(product => {
+                                return <tr>
+                                    <td>{product.id}</td>
+                                    <td>{product.categoryId}</td>
+                                    <td>{product.productName}</td>
+                                    <td>{product.quantityPerUnit}</td>
+                                    <td>{product.unitPrice}</td>
+                                    <td>{product.unitsInStock}</td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </Table>
             </div>
         )
     }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
-        currentCategory:state.changeCategoryReducer,
-        products:state.productListReducer
+        currentCategory: state.changeCategoryReducer,
+        products: state.productListReducer
     }
 }
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
     return {
-        actions:{
-            getProducts:bindActionCreators(productActions.getProducts,dispatch)
+        actions: {
+            getProducts: bindActionCreators(productActions.getProducts, dispatch)
         }
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(ProductList)
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList)
