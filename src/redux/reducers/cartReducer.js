9d176f7c14fbc699730d3085a03133c568322bbf
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-25 21:30:36 +0300

    addToCartReducer

---
+++ src/redux/reducers/cartReducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from '../actions/actionTypes'
+import initialState from './initialState'
+export function addToCartReducer(state = initialState.cart, action) {
+    switch (action.type) {
+        case actionTypes.ADD_TO_CART:
+            return state;
+        default:
+            return state;
+    }
+}
