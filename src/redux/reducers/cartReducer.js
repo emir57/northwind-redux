3e24a5a517a2d5a4ebf87e45320e30579aa7aecf
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-25 21:37:26 +0300

    cartReducer

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -3,6 +3,11 @@ import initialState from './initialState'
 export function cartReducer(state = initialState.cart, action) {
     switch (action.type) {
         case actionTypes.ADD_TO_CART:
+            let index = state.cart.findIndex(x => x.product.id === action.payload.id);
+            if(index === -1)
+                state.cart.push(action.payload);
+            else
+                state.cart[index].quantity+=1;
             return state;
         default:
             return state;
