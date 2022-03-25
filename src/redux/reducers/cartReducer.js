f1829d4a841c8f15e137649b5f76c947f1bf1134
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-25 21:31:15 +0300

    refactoring

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,6 +1,6 @@
 import * as actionTypes from '../actions/actionTypes'
 import initialState from './initialState'
-export function addToCartReducer(state = initialState.cart, action) {
+export function cartReducer(state = initialState.cart, action) {
     switch (action.type) {
         case actionTypes.ADD_TO_CART:
             return state;
