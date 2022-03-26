c51e47d9277765b157caf4a0277e35ca8a794333
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-26 21:35:55 +0300

    refactoring variables

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -4,13 +4,13 @@ export default function cartReducer(state = initialState.cart, action) {
     switch (action.type) {
         case actionTypes.ADD_TO_CART:
             let name = "";
-            if(action.payload.productName.length > 6){
-                action.payload.productName=action.payload.productName.substr(0,6)+"...";
+            if (action.payload.productName.length > 6) {
+                action.payload.productName = action.payload.productName.substr(0, 6) + "...";
             }
             let item = state.find(x => x.product.id === action.payload.id);
             if (!item) {
                 state = state.slice();
-                state.push(Object.assign({quantity:1,product:action.payload}));
+                state.push(Object.assign({ quantity: 1, product: action.payload }));
                 return state;
             }
             else {
@@ -24,11 +24,11 @@ export default function cartReducer(state = initialState.cart, action) {
             }
         case actionTypes.REMOVE_FROM_CART:
             state = state.slice();
-            let getindex = state.findIndex(x=>x.product.id === action.payload.id);
-            if(state[getindex].quantity > 1){
-                state[getindex].quantity -= 1;
-            }else{
-                state.splice(getindex,1);
+            let index = state.findIndex(x => x.product.id === action.payload.id);
+            if (state[index].quantity > 1) {
+                state[index].quantity -= 1;
+            } else {
+                state.splice(index, 1);
             }
         default:
             return state;
