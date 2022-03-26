3a693d3e88938e18431c987669a82634ab0a058f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-26 14:55:06 +0300

    Add CartSummary Component

---
+++ src/components/cart/CartSummary.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CartSummary extends Component {
+    render() {
+        return (
+            <div>CartSummary</div>
+        )
+    }
+}
