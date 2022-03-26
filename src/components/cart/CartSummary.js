a965cfe6d0a0d5aa25005702b1a8d8c98524ea61
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-26 15:06:14 +0300

    get carts

--- src/components/cart/CartSummary.js
+++ src/components/cart/CartSummary.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import cartReducer from '../../redux/reducers/cartReducer'
+import * as cartActions from '../../redux/actions/cartActions'
 
 class CartSummary extends Component {
-    componentDidMount(){
-        
+    componentDidMount() {
+
     }
     render() {
         return (
@@ -19,13 +21,22 @@ class CartSummary extends Component {
                     Cart
                 </DropdownToggle>
                 <DropdownMenu end>
-                    <DropdownItem>
-                        Option 1
-                    </DropdownItem>
+                    {
+                        this.props.carts.map(cart => {
+                            return <DropdownItem key={cart.product.id}>
+                                {cart.product.productName} | x{cart.quantity}
+                            </DropdownItem>
+                        })
+                    }
                 </DropdownMenu>
             </UncontrolledDropdown>
         )
     }
 }
+function mapStateToProps(state) {
+    return {
+        carts: state.cartReducer
+    }
+}
 
-export default connect()(CartSummary)
+export default connect(mapStateToProps)(CartSummary)
