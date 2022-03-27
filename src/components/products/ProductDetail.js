b15792f94b6129752b54e1cca950feccce67e1a5
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-27 20:06:19 +0300

    TextInput

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -1,1 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { TextInput } from '../toolbox/TextInput'
+
+const ProductDetail = (
+    categories,
+    product,
+    onSave, onChange
+) => {
+    return (
+        <form onSubmit={onSave}>
+            <h2>{product.id ? "Güncelle" : "Ekle"}</h2>
+            <TextInput name="productName" label="Product Name" value={product.productName} onChange={onChange} />
+        </form>
+    )
+} 
