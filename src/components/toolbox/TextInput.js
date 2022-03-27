2b0bd13d90dfe092712cd40f5cbc679eea00ec7d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-27 18:37:29 +0300

    add div className

--- src/components/toolbox/TextInput.js
+++ src/components/toolbox/TextInput.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const TextInput = ({ name, label, onChange, placeHolder, value, error }) => {
-
+    let wrapperClass = "form-group"
+    if (error && error.length > 0) {
+        wrapperClass += " has-error";
+    }
+    return (
+        <div className={wrapperClass}>
+            
+        </div>
+    )
 }
 
 export default TextInput
