fe56d3729a8c0370b4d4c3bfd81bc16f131a921a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-29 15:01:59 +0300

    NotFound Component

---
+++ src/components/common/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className='display-2 mx-auto'>Sayfa Bulunamadı</h2>
+            </div>
+        )
+    }
+}
